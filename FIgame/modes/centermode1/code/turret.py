a9535876c1519ab6fbad90acf55a03207f6c9784
Unknown <shastada@gmail.com>
2018-01-21 15:32:05 -0800

    attempting a quick trough thing

---
+++ FIgame/modes/centermode1/code/turret.py
@@ -0,0 +1,29 @@
+from mpf.core.mode import Mode
+
+
+class Tyrret(Mode):
+
+    def mode_init(self):
+        print("My custom mode code is being initialized")
+
+    def mode_start(self, **kwargs):
+        # The mode_start method needs **kwargs because some events that
+        # start modes pass additional parameters
+
+        print("My custom mode code is starting")
+
+        self.add_mode_event_handler('stepper_center_turret_ready', self.ejectTheBall)
+
+
+    def my_callback(self):
+        print("My delayed call was just called!")
+
+    def ejectTheBall(self, **kwargs):
+        print("Ejecting..")
+        self.machine.ball_devices.bd_trough.eject()
+        
+    def mode_stop(self, **kwargs):
+        # The mode_stop method needs **kwargs because some events that
+        # stop modes pass additional parameters
+
+        print("My custom mode code is stopping")
